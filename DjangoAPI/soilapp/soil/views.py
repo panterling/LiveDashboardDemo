e196d893ea4781e2cdaac17f168fae69c3b1aa9b
root <chris.pantling@btinternet.com>
2018-07-05 07:14:52 +0000

    migrate docker config to mounter volume

--- DjangoAPI/soilapp/soil/views.py
+++ DjangoAPI/soilapp/soil/views.py
@@ -15,7 +15,7 @@ import time
 
 def getConnectionString():
 ## LOAD CONFIG ##
-    CONF = yaml.load(open("../config.yml", "r"))
+    CONF = yaml.load(open("/var/config/config.yml", "r"))
     DB_HOST = str(CONF["server_ip"])
     DB_NAME = str(CONF["postgres_db"])
     DB_USERNAME = str(CONF["postgres_username"])
