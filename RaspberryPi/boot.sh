ccf0dbad7421f6c83d96a3577d5c02f5325dba6e
root <chris.pantling@btinternet.com>
2018-08-01 19:40:22 +0000

    changes to reflect new PCB

--- RaspberryPi/boot.sh
+++ RaspberryPi/boot.sh
@@ -3,3 +3,5 @@
 screen -d -m -L -S sensorBoot bash -c "cd /home/pi/LiveDashboardDemo/RaspberryPi && python3 kafkaSoilProducer.py";
 screen -d -m -L -S remoteControlBoot bash -c "cd /home/pi/LiveDashboardDemo/RaspberryPi && python3 remoteControlServer.py";
 
+# NOIP daemon
+/usr/local/bin/noip2
